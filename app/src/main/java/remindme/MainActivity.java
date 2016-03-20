3c0d519ad4f57f75da4680c4e4d1158cd844695a
MD54RUS <pro-rock@ngs.ru>
2016-03-20 22:28:09 +0600

    Create MainActivity and layout

---
+++ app/src/main/java/remindme/MainActivity.java
@@ -0,0 +1,17 @@
+package remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.pro_rock.remindme.R;
+
+/**
+ * Created by Pro-rock on 20.03.2016.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
