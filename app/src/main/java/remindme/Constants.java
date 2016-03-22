504cda5d3a74648e351da88a3ce31446c0139901
MD54RUS <pro-rock@ngs.ru>
2016-03-22 14:45:01 +0600

    Added NavigationView

---
+++ app/src/main/java/remindme/Constants.java
@@ -0,0 +1,12 @@
+package remindme;
+
+/**
+ * Created by Pro-rock on 22.03.2016.
+ */
+public class Constants {
+
+    public static final int TAB_ONE = 0;
+    public static final int TAB_TWO = 1;
+    public static final int TAB_THREE = 2;
+
+}
